2561880ca8e2c7a3e1f97fbfb45d08f903a1bede
Stanislas <stanislas.descamps@gmail.com>
2016-03-15 21:06:49 +0100

    Ajout de l'attribut losodispo

--- BattleClient/src/Logos.java
+++ BattleClient/src/Logos.java
@@ -4,11 +4,13 @@ public class Logos {
 	private String idLogo;
 	private int logPositionX;
 	private int logPositionY;
+	private boolean estDispo;
 
-	public Logos(String idLogo, int logPosX, int logPosY){
+	public Logos(String idLogo, int logPosX, int logPosY, boolean estDispo){
 		this.idLogo=idLogo;
 		this.logPositionX=logPosX;
 		this.logPositionY=logPosY;
+		this.estDispo=estDispo;
 	}
 		
 	public String getIdLogo() {
@@ -33,6 +35,14 @@ public class Logos {
 
 	public void setLogPositionY(int logPositionY) {
 		this.logPositionY = logPositionY;
+	}
+
+	public boolean isEstDispo() {
+		return estDispo;
+	}
+
+	public void setEstDispo(boolean estDispo) {
+		this.estDispo = estDispo;
 	}
 	
 }
